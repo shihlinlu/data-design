85f1a5e296668f7b7e37bb4dd40812b245e5d8a2
Shihlin Lu <shihlinlu@gmail.com>
2017-04-13 10:07:19 -0600

    Detailed out conceptual model.

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -10,6 +10,30 @@
 		</header>
 
 		<main>
+			<h3>Entities</h3>
+			<ul>
+				<li>Profile</li>
+				<li>Product</li>
+				<li>Order</li>
+			</ul>
+			<h3>Attributes</h3>
+			<ul>
+				<li>profileEmail</li>
+				<li>profileID</li>
+				<li>profileUsername</li>
+				<li>profileLocation</li>
+				<li>profileJoinDate</li>
+				<li>profileBio</li>
+				<li>profileImage</li>
+				<li>profileFavoriteItems</li>
+				<li>profileItems</li>
+				<li>profileAnnouncements</li>
+			</ul>
+			<h3>Relationships</h3>
+			<ul>
+				<li>One user can favorite once for any single furniture product</li>
+				<li>Many users can favorite any furniture products</li>
+			</ul>
 
 		</main>
 	</body>
