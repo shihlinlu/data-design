5da0a225189f0b28c8473ba291807dd8ec7834ce
Shihlin Lu <shihlinlu@gmail.com>
2017-04-13 10:24:12 -0600

    updated conceptual model

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -10,14 +10,22 @@
 		</header>
 
 		<main>
+			<h2>Entities and Attributes</h2>
 			<h3>Entities</h3>
 			<ul>
 				<li>Profile</li>
 				<li>Product</li>
 				<li>Order</li>
 			</ul>
-			<h3>Attributes</h3>
+			<p>Subject: Profile</p>
+			<p>Verb: Favorites</p>
+			<p>Object: Furniture Items</p>
+			<h3>Profile</h3>
 			<ul>
+				<li>profileID</li>
+				<li>profileHash</li>
+				<li>profileSalt</li>
+				<li>profileActivationToken</li>
 				<li>profileEmail</li>
 				<li>profileID</li>
 				<li>profileUsername</li>
@@ -27,12 +35,33 @@
 				<li>profileImage</li>
 				<li>profileFavoriteItems</li>
 				<li>profileItems</li>
+				<li>profileItemCost</li>
 				<li>profileAnnouncements</li>
 			</ul>
+			<h3>Items</h3>
+			<ul>
+				<li>itemID</li>
+				<li>itemType</li>
+				<li>itemStyle</li>
+				<li>itemDescription</li>
+				<li>itemName</li>
+				<li>itemCost</li>
+				<li>itemInventory</li>
+				<li>itemPostDate</li>
+			</ul>
+			<h3>Favorite</h3>
+			<ul>
+				<li>favoriteProfileID</li>
+				<li>favoriteDate</li>
+				<li>favoriteItemID</li>
+			</ul>
+
+
 			<h3>Relationships</h3>
 			<ul>
 				<li>One user can favorite once for any single furniture product</li>
 				<li>Many users can favorite any furniture products</li>
+				<li>Many furniture products can be favorited many times</li>
 			</ul>
 
 		</main>
--- epic/use-case.php
+++ epic/use-case.php
@@ -14,7 +14,13 @@
 			<p>
 				It's Monday, 8:30am and Nancy has a furniture design review meeting at 1pm. She needs to finalize the list of furniture products that will fit well with the futuristic living room collection. This will enable the product design team to see if their next batch of living room furniture products are in align with company design standards and if it makes sense for the overall futuristic collection theme.
 			</p>
-			<p>Nancy proceeds to login to contempodesign.com, navigates to the living room furniture section, clicks on the black, faux leather adjustable chair, then clicks on the heart button, which indicates that it is a favorite item.</p>
+			<h3>What's next:</h3>
+			<ol>
+				<li>Nancy logins to contempodesign.com</li>
+				<li>Navigates to the living room furniture section</li>
+				<li>Clicks on the black, faux leather adjustable chair item</li>
+				<li>Clicks on the favorite button, which stores the item in the favorites section of her account.</li>
+			</ol>
 
 		</main>
 
