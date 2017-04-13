314ffa98d044ba817a7fe87f9dca2c9f3797f28f
Shihlin Lu <shihlinlu@gmail.com>
2017-04-13 09:24:03 -0600

    Elaborated on persona, consisting of the name, age, profession, technology used, attitudes and behaviors, frustration and needs, and goals.

--- epic/persona.php
+++ epic/persona.php
@@ -8,8 +8,33 @@
 		<header>
 			<h1>PERSONA</h1>
 		</header>
-
 		<main>
+			<h3>Name: Nancy</h3>
+			<h3>Age: 26</h3>
+			<h3>Profession:</h3>
+			<p>Nancy is a senior industrial designer at contempodesign.com, a contemporary furniture design company that sells exclusively online. All furniture products are designed and manufactured in-house, eliminating the need for extensive supply chains and manufacturer contracts. Contempo Design also invites customers to submit their own furniture ideas for future products. Their website is an online eCommerce platform where customers can purchase furniture online, as well as trade existing furniture for online credit towards new furniture purchases.
+			</p>
+			<h3>Technology:</h3>
+			<p>
+				Nancy is a all-Mac-or-nothing user and uses the following machines:
+			</p>
+			<ul>
+				<li>15 inch MacBook Pro</li>
+				<li>27 inch iMac with 5K Retina Display</li>
+				<li>iPhone 7 Plus with unlimited 4G data</li>
+				<li>12.9 inch iPad Pro with unlimited 4G data</li>
+			</ul>
+			<h3>Attitudes and Behaviors:</h3>
+			<p>
+				Nancy has a busy work schedule and is meticulous about being efficient with all tasks and projects. If a team member wants to discuss about ergonomics of a Herman Miller office chair (and discuss how Contempo Design can create an even better chair) she will gladly set up a meeting; otherwise, a meeting about discussing fabric patterns will be directed to Slack. Nancy enjoys Pinterest and seeks ideas from the latest user-curated furniture boards, but does not spend more than 1 hour a day choosing products that fit similar company design standards.
+			</p>
+			<h3>Frustrations and Needs:</h3>
+			<p>Nancy needs to create furniture collections for three major groups of people: 1) future customers to get inspiration for whole-room furniture setups and 2) investors so that they can assess the product traction and company growth. Since Nancy is a key decision maker for designing upcoming furniture products, she works closely with a data analyst to see the products that customers are viewing and purchasing. They also send extensively detailed customer feedback surveys. An additional metric that would be useful is being able to track products that customers like, but do not have immediate plans of purchasing. This will also allow Nancy and the rest of the Contempo Design team to view most popular items from furniture collections.</p>
+			<h3>Goals:</h3>
+			<ol>
+				<li>In less than 1 minute, obtain the ability to easily "favorite" individual or groups of furniture products.</li>
+				<li>View customer's public favorite furniture products.</li>
+			</ol>
 
 		</main>
 	</body>
